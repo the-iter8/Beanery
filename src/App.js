c76bab06e42c62e66170ac1cbfa66c4a58e3882a
iter8 <randomdweller.me@gmail.com>
2022-07-29 05:58:53 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import menu from "./data";
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import Story from "./Components/Story";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import { useReducer, useState } from "react";
+
+export const CartValue = React.createContext();
 
 function App() {
+  const [totalCartValue, setTotalCartValue] = useState(0);
+  const reducer = (state, action) => {
+    var item = 0;
+    switch (action.type) {
+      case "ADD":
+        item = menu[action.payload - 1];
+        setTotalCartValue(totalCartValue + item.price);
+        return [...state, item];
+
+      case "REMOVE":
+        item = state[action.payload]; 
+        if (state.length !== 1) {
+          setTotalCartValue(totalCartValue - item.price);
+          const cart = [...state];
+          cart.splice(action.payload, 1);
+          return [...cart];
+        } else {
+          setTotalCartValue(0);
+          return [];
+        }
+
+      default:
+        throw new Error();
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CartValue.Provider value={{ reducer, dispatch, state, totalCartValue }}>
+        <Home></Home>
+        <Menu></Menu>
+        <Story></Story>
+        <Contact></Contact>
+        <Footer></Footer>
+      </CartValue.Provider>
+    </>
   );
 }
 
